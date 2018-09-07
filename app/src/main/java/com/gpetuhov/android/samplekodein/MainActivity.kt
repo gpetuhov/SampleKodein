76e8e58d1c438e23d7d93817937dcfe4a75ea292
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-09-07 11:05:10 +0300

    Trigger injection in onCreate()

--- app/src/main/java/com/gpetuhov/android/samplekodein/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplekodein/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.gpetuhov.android.samplekodein.repository.Repository
 import kotlinx.android.synthetic.main.activity_main.*
 import org.kodein.di.KodeinAware
+import org.kodein.di.KodeinTrigger
 import org.kodein.di.android.closestKodein
 import org.kodein.di.generic.instance
 
@@ -23,8 +24,19 @@ class MainActivity : AppCompatActivity(), KodeinAware {
     // Concrete implementation is injected by Kodein.
     private val repo: Repository by instance()
 
+    // We may leave everything like in the code above and it will be enough.
+    // Repository will by initialized lazily.
+    // But to be sure everything's working fine it is better
+    // to trigger injection in onCreate. To do this, override kodeinTrigger property:
+    override val kodeinTrigger = KodeinTrigger()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Trigger injections here before anything is used.
+        // (This is like SampleKodeinApp.getAppComponent().inject(this) in Dagger)
+        kodeinTrigger.trigger()
+
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
